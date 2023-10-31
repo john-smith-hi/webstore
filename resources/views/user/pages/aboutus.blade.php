f7a3db80a44885b030d5c97f26671ecf7a0eed2f
leminhnhat <mike841651616@gmail.com>
2023-11-01 01:21:44 +0700

    fef

--- resources/views/user/pages/aboutus.blade.php
+++ resources/views/user/pages/aboutus.blade.php
@@ -8,7 +8,7 @@
     use App\Models\Aboutus;
     $aboutus = Aboutus::orderBy('id','desc')->first();
 @endphp
-@if(!empty($aboutus) && count($aboutus) > 0)
+@if(!empty($aboutus))
     <div class="container-fluid pt-5">
         <div class="text-center mb-4">
             <h2 class="section-title px-5"><span class="px-2">Về chúng tôi</span></h2>
