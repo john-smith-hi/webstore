e8b004fb7472647f8110c3d141701c1d1275aef2
leminhnhat <mike841651616@gmail.com>
2023-11-06 21:33:12 +0700

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,16 +15,7 @@ use App\Models\Category;
 class TestController extends Controller
 {
     public function index(){
-        $product_image = ProductImage::first();
-        $product = Product::find($product_image->product_id);
-        $category = Category::find($product->category_id);
-        $child_categories = Category::where('parent_id', $product->category_id)->get();
-        foreach($child_categories as $child_category){
-            $child_products = Product::where('category_id', $child_category->id)->get();
-            foreach($child_products as $child_product){
-                echo $child_product->id.' : '.$child_product->name.'<br>';
-            }
-        }
+        dd(ProductImage::inRandomOrder()->take(3));
     }
 
 }
